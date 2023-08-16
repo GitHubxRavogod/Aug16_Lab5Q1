fd9546015169d05c3eed0c7156f2050a0f49ee68
GitHubxRavogod <86479020+GitHubxRavogod@users.noreply.github.com>
2023-08-16 16:42:58 +0800

    Lab5Q1.py

--- Lab5Q1.py
+++ Lab5Q1.py
@@ -5,8 +5,6 @@ filename: circleArea.py
 This program asks the user to input a               
 number for the radius of a circle.  The program     
 then calculates and output the area of the circle.
-'''
-'''
 import math
 import sys
 radius = float(input( "Enter radius in feet : " )) 
@@ -23,7 +21,9 @@ Modify the program circleArea.py given above to make use of this function.
 '''
 import math
 import sys
+def computeArea(radius)
+  return math.pi * radius ** 2
 radius = float(input( "Enter radius in feet : " )) 
 area = math.pi * radius ** 2
 sys.stdout.write("The radius you provided was " + format(radius,'.2f') +
-                " feet and the area is about " + format(area,'.2f') + " sq feet" )
+                " feet and the area is about " + format(computeArea(radius),'.2f') + " sq feet" )
